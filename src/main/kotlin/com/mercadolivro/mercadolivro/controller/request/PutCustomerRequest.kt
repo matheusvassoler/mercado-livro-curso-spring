e6498a9bcfd8723870e8f01cb977df2f58b9b941
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2022-01-03 14:31:05 -0300

    Criação da camada service

--- src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PutCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -1,7 +1,8 @@
 package com.mercadolivro.mercadolivro.controller.request
 
+import com.mercadolivro.mercadolivro.model.CustomerModel
+
 data class PutCustomerRequest(
     var name: String,
-
     var email: String
 )
